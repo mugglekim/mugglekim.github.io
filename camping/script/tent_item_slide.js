2fdd60dd12500a41338f00f1f55b455373438ca8
mugglekim <shalomseonmi@gmail.com>
2025-02-12 14:55:54 +0900

    edit camping

--- camping/script/tent_item_slide.js
+++ camping/script/tent_item_slide.js
@@ -1,24 +1,23 @@
 const prevBtn=document.querySelector('.item-btn>img:nth-child(1)');
 const nextBtn=document.querySelector('.item-btn>img:nth-child(2)');
-const itemUl=document.querySelector('ul.item-box');
+const slideWrap=document.querySelector('.box-slide');
+const slideUl=document.querySelector('ul.item-box');
 const itemLi=document.querySelector('ul.item-box>li');
 
-//변수설정
-let current=0;
-const SLIDE_COUNT=itemUl.childElementCount; //8
-const TOTAL=SLIDE_COUNT-2;
-const slide_WIDTH=(document.querySelector('.box-slide').clientWidth)/3;
-console.log(slide_WIDTH);
+let current=1;
+const SLIDE_COUNT=slideUl.childElementCount; //8
+const TOTAL=SLIDE_COUNT+2;
+const slide_WIDTH=itemLi+(document.querySelector('.slideWarp').clientWidth*0.01);
 
 //슬라이드 액션
 const SlideAct=(idx)=>{
-  itemUl.style.transition='0.5s';
-  itemUl.style.transform=`translateX(-${slide_WIDTH*idx}px)`;
+  slideUl.style.transition='0.5s';
+  slideUl.style.transform=`translateX(-${slide_WIDTH*idx}px)`;
 }
 const resetSlideAct=(idx)=>{
   setTimeout(()=>{
-    itemUl.style.transition='none';
-    itemUl.style.transform=`translateX(-${slide_WIDTH*idx}px)`;
+    slideUl.style.transition='none';
+    slideUl.style.transform=`translateX(-${slide_WIDTH*idx}px)`;
   },500);
 }
 
@@ -52,30 +51,16 @@ nextBtn.addEventListener('click',handerNextBtn);
 //슬라이드 컨텐츠 앞뒤로 클론 붙이기
 const cloneSlide=()=>{
   //자식들에 대한 객체
-  const slideGroup=itemUl.children;
+  const slideGroup=slideUl.children;
   const first=slideGroup[0].cloneNode(true);
   const last=slideGroup[SLIDE_COUNT-1].cloneNode(true);
   //1번 앞에 5번이 오도록
-  itemUl.prepend(last);
+  slideUl.prepend(last);
   //5번 뒤에 1번이 오도록
-  itemUl.append(first);
-  
+  slideUl.append(first);
 }
 
-//슬라이드 width 값을 css에서 가져오기
-const setCssValue=()=>{
-  //css의 변수값 가져오기
-  let result=getComputedStyle(document.documentElement).getPropertyValue('--count');
-  document.documentElement.style.setProperty('--count',`${TOTAL}`);
-}
-
-
-//초기값
 const init=()=>{
-  //처음 실행되는 함수
   cloneSlide();
-  setCssValue();
-  //슬라이드 초기 위치 설정
-  itemUl.style.transform=`translateX(-${slide_WIDTH}px)`;
 }
 window.onload=init;
