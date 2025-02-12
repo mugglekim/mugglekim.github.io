68fbf2f6f7da26ddffc338657ce09b3a7331a9d7
mugglekim <shalomseonmi@gmail.com>
2025-02-12 17:24:25 +0900

    edit camping

--- camping/script/tent_item_slide.js
+++ camping/script/tent_item_slide.js
@@ -1,24 +1,22 @@
 const prevBtn=document.querySelector('.item-btn>img:nth-child(1)');
 const nextBtn=document.querySelector('.item-btn>img:nth-child(2)');
-const slideWrap=document.querySelector('.box-slide');
-const slideUl=document.querySelector('ul.item-box');
-const itemLi=document.querySelector('ul.item-box>li');
+const imgSlider=document.querySelector('ul.item-box');
+const slideGroup=imgSlider.children;
 
-let current=1;
-const SLIDE_COUNT=slideUl.childElementCount; //8
-const TOTAL=SLIDE_COUNT+2;
-const slide_WIDTH=itemLi+(document.querySelector('.slideWarp').clientWidth*0.01);
+let current=0;
+const SLIDE_COUNT=imgSlider.childElementCount/3; //8
+const IMG_WIDTH=document.querySelector('ul.item-box').clientWidth/SLIDE_COUNT;
 
 //슬라이드 액션
 const SlideAct=(idx)=>{
-  slideUl.style.transition='0.5s';
-  slideUl.style.transform=`translateX(-${slide_WIDTH*idx}px)`;
+  imgSlider.style.transition='0.3s';
+  imgSlider.style.transform=`translateX(-${IMG_WIDTH*idx}px)`;
 }
 const resetSlideAct=(idx)=>{
   setTimeout(()=>{
-    slideUl.style.transition='none';
-    slideUl.style.transform=`translateX(-${slide_WIDTH*idx}px)`;
-  },500);
+    imgSlider.style.transition='0.3s';
+    imgSlider.style.transform=`translateX(-${IMG_WIDTH*idx}px)`;
+  },100);
 }
 
 //prev 버튼 클릭 핸들러
@@ -28,8 +26,8 @@ prevBtn.addEventListener('click',()=>{
   // if(current===0){
   //   current=SLIDE_COUNT;
   // }
-  if(current<1){
-    current=SLIDE_COUNT;
+  if(current<0){
+    current=SLIDE_COUNT-1;
     resetSlideAct(current);
   }
 });
@@ -41,26 +39,9 @@ const handerNextBtn=()=>{
   // if(current===TOTAL-1){
   //   current=1;
   // }
-  if(current>SLIDE_COUNT){
-    current=1;
+  if(current>=SLIDE_COUNT){
+    current=0;
     resetSlideAct(current);
   }
 }
 nextBtn.addEventListener('click',handerNextBtn);
-
-//슬라이드 컨텐츠 앞뒤로 클론 붙이기
-const cloneSlide=()=>{
-  //자식들에 대한 객체
-  const slideGroup=slideUl.children;
-  const first=slideGroup[0].cloneNode(true);
-  const last=slideGroup[SLIDE_COUNT-1].cloneNode(true);
-  //1번 앞에 5번이 오도록
-  slideUl.prepend(last);
-  //5번 뒤에 1번이 오도록
-  slideUl.append(first);
-}
-
-const init=()=>{
-  cloneSlide();
-}
-window.onload=init;
